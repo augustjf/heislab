5d230938e3d85f110cf6c32bdc9cafdf85946077
August Fors <augustjf@stud.ntnu.no>
2022-02-24 09:24:30 +0100

    started queue module

--- source/queue_module.c
+++ source/queue_module.c
@@ -1,62 +1,7 @@
-
 #include "queue_module.h"
 
-void stop(){
-    int stop_yn = elevio_stopButton();
-    if(stop_yn == 1){
-        elevio_stopLamp(1);
-        elevio_motorDirection(0);
-    } 
-}
 
-void run_elevator(){
 
-    //mulige states
-    typedef enum { 
-    INIT   = 0,
-    STANDBY   = 1,
-    FLOOR_REACHED = 2,
-    STOP = 3,
-    } ELEV_STATE;
-    
-    ELEV_STATE state = INIT;  //settes automatisk til init
-
-    //looper gjennom
-    while(1) {
-        
-
-        if(elevio_stopButton() == 1){  //må alltid sjekke om det er stop
-            state = STOP;
-        }
-
-        switch(state){
-            case INIT:
-                if (elevio_floorSensor == -1) {
-                    elevio_motorDirection(-1);
-                }
-
-                while (elevio_floorSensor == -1){ //waits for elevator to reach a floor
-                }
-
-                elevio_motorDirection(0);
-                state = STANDBY;
-                break;
-            
-            case STANDBY:
-                //check vector
-                //drive elevator
-                break;
-
-            case STOP:
-                elevio_stopLamp(1);
-                elevio_motorDirection(0);
-                //delete queue
-                break;
-
-            default:
-                break;
-        }
-    
-    }
+void add_order(ELEV_STATE state, MotorDirection dirn, int current_floor, int order_floor, ButtonType btn) {
 
 }
--- source/queue_module.h
+++ source/queue_module.h
@@ -1,34 +1,3 @@
 #pragma once
 #include "driver/elevio.h"
-/**
- * @file
- * @brief 
- * Library for queue and overall control
- */
-
-/**
- * @brief 
- * Structure for holding array of order and other data related to the elevator
- * and its movement
- */
-struct Queue{
-    int orders[100];
-    int ready;          //door closed and ready to move to new floor?
-    int elev_floor;
-    int state_motor;
-    int next_floor;
-};
-
-
-/**
- * @brief Read stop button from elevator, set motor drive to zero
- * 
- */
-int check_stop();
-
-
-/**
- * @brief 
- * 
- */
-void run_elevator();
+#include "control_module.h"
