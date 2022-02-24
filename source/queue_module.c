ba0b8fb01abb16fd0fee897d304af37b5dc8c6df
August Fors <augustjf@stud.ntnu.no>
2022-02-24 09:51:32 +0100

    include errors

---
+++ source/queue_module.c
@@ -0,0 +1,7 @@
+#include "queue_module.h"
+
+
+
+void add_order(ELEV_STATE state, MotorDirection dirn, int current_floor, int order_floor, ButtonType btn) {
+
+}
--- source/queue_module.h
+++ source/queue_module.h
@@ -1,8 +1,10 @@
 #pragma once
+#include "driver/elevio.h"
+#include "control_module.h"
 
 struct Queue{
     int orders[100];
-    bool ready;
+    int ready;
     int elev_floor;
     int state_motor;
     int next_floor;
